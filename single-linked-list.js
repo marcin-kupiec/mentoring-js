9845dc25177c81abb21e0c7efb1f0aa100904115
mayha <machluchachlumaria@gmail.com>
2022-11-18 15:42:24 +0100

    implementation of SLinkedList and Node classes

--- single-linked-list.js
+++ single-linked-list.js
@@ -3,13 +3,29 @@
 class SLinkedList {
   constructor() {
     // provide basic variables and initialize them
+
+    //this class should have the address of the first list node - to the head
+    let head = null; //initilazing the list first, not adding the comments
+    let end = this.head;
   }
 
   // append function takes one parameter:
   //  * data: type any
   // returns nothing
   // adds new data to the end of the linked list
-  append(data) {}
+  append(data) {
+    
+    if(!head) {
+      this.head = new Node(data);
+      //end now should point on the new node created
+    }
+    let node = new Node(data);
+    let previous = this.end;
+    node.setNext = this.end;
+    previous.setNext = node;
+    
+
+  }
 
   // addAt function takes two parameters:
   //  * id: type integer (index where to insert new data)
@@ -40,13 +56,21 @@ class SLinkedList {
 // structure to implement node of the list that keeps data
 // should have some properties e.g. data or reference to the next node
 class Node {
-  constructor(data) {}
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 
   // returns reference to the next node of the list
-  getNext() {}
+  getNext() {
+    return this.next;
+  }
 
   // sets reference to the next node of the list
-  setNext(node) {}
+  setNext(node) {
+    //is this a place to do the check or casting to the proper type to check if we have reference?
+    node.
+  }
 }
 
 export default SLinkedList;
